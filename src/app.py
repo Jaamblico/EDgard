def4bb875fcc7af808e11092bc3f8c0bb41ea20b
Ariel Silva <ariel.silva@pedidosya.com>
2024-07-24 14:02:36 -0300

    feat: adds chat engine

--- src/app.py
+++ src/app.py
@@ -1,22 +1,33 @@
 import gradio as gr
+from respond import respondFn
 from upload import upload_file
-from respond import respond
 from utils import js_func
 
 # Crear interfaz de Gradio
 with gr.Blocks(js=js_func) as demo:
-    gr.Markdown("## File Upload Interface")
-    upload_button = gr.UploadButton("Click to Upload a File", file_types=["csv", "pdf", "txt", "docx", "json"], file_count="multiple")
-    file_output = gr.Textbox(label="File Upload Status")
+    gr.Markdown("## Subir archivos")
+    upload_button = gr.UploadButton(
+        "Seleccionar archivos",
+        file_types=["csv", "pdf", "txt", "doc", "docx", "json", "xls", "xlsx"],
+        file_count="multiple",
+    )
+    file_output = gr.Textbox(label="Estado de carga:")
     upload_button.upload(upload_file, upload_button, file_output)
 
     gr.Markdown("## Query Interface")
     chatbot = gr.Chatbot()
-    query_input = gr.Textbox(placeholder="Ingrese su pregunta aquí...", show_label=False)
+    query_input = gr.Textbox(
+        placeholder="Ingrese su pregunta aquí...", show_label=False
+    )
     state = gr.State([])
-    query_input.submit(respond, inputs=[query_input, state], outputs=[chatbot, query_input, state])
+    query_input.submit(
+        respondFn, inputs=[query_input, state], outputs=[chatbot, query_input, state]
+    )
     send_button = gr.Button("Enviar")
-    send_button.click(respond, inputs=[query_input, state], outputs=[chatbot, query_input, state])
+    send_button.click(
+        respondFn, inputs=[query_input, state], outputs=[chatbot, query_input, state]
+    )
+
 
 if __name__ == "__main__":
     demo.launch()
--- src/upload.py
+++ src/upload.py
@@ -1,19 +1,13 @@
 import shutil
-from utils import initial_prompt
-from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
-from llama_index.core.response_synthesizers import ResponseMode, get_response_synthesizer
 
-# Configure response synthesizer
-response_synthesizer = get_response_synthesizer(response_mode=ResponseMode.REFINE)
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
+
 
 def upload_file(files):
     for file in files:
-        shutil.copy(file, dst='./data')
+        shutil.copy(file, dst="./data")
     global documents, index, query_engine
-    documents = SimpleDirectoryReader("data").load_data()
+    # Load documents and create index
+    documents = SimpleDirectoryReader("./data").load_data()
     index = VectorStoreIndex.from_documents(documents)
-    query_engine = index.as_query_engine(
-        response_synthesizer=response_synthesizer,
-        prompt_template=initial_prompt
-    )
-    return "Files uploaded successfully!"
+    return "Archivo subido correctamente."
